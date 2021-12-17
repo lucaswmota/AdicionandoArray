7f41659a317f4449ae09bd6d3c9e7d45073aaa14
Lucas Mota <lucasmota7@gmail.com>
2021-12-16 21:59:56 -0300

    Add project files.

---
+++ Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdicionandoArray
+{
+    internal class Pessoa
+    {
+        string nome { get; set; }
+        int idade { get; set; }
+        double peso { get; set; }
+
+        public void cadastrarPessoa()
+        {
+            Console.Write("Informe o Nome da Pessoa: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe a Idade da Pessoa: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o Peso da Pessoa: ");
+            peso = double.Parse(Console.ReadLine());
+        }
+
+        public void exibirPessoa()
+        {
+            Console.WriteLine($"Nome da Pessoa: {nome}" +
+                $"\nIdade {idade}" +
+                $"\nPeso: {peso}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace AdicionandoArray
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            ArrayList lista = new ArrayList();
+
+            string resposta;
+
+            do
+            {
+                Console.Clear();
+                Pessoa pessoa = new Pessoa();
+                pessoa.cadastrarPessoa();
+                lista.Add(pessoa);
+
+                Console.Write("Deseja Cadastrar outra pessoa?");
+                resposta = Console.ReadLine().ToUpper();
+
+            }
+            while (resposta == "S");
+
+            foreach(Pessoa pessoa in lista)
+            {
+                pessoa.exibirPessoa();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
